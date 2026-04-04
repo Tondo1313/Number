862a5d4ef3456105a3a65547128bbf2fbc0e4dba
Tondo <aliss.kuper2@gmail.com>
2026-04-05 00:22:10 +0300

    chmod +x ./Number.sh

---
+++ Number.sh
@@ -0,0 +1,7 @@
+echo "Welcome to Number OT. Please wait, I'm preparing your system for work..."
+cd ~/Documents || exit
+mkdir -p number
+cd number || exit
+touch assumptions.txt logical_connections.txt social_networks.txt maybe.txt
+mkdir leads
+echo "your system was done for osint working. Look at the /Documents"
