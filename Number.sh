fe77ff86fe825a54cf6d42a035709ef456cd9007
Tondo <aliss.kuper2@gmail.com>
2026-04-06 16:49:45 +0300

    Update Number.sh
    added licence

--- Number.sh
+++ Number.sh
@@ -3,5 +3,6 @@ cd ~/Documents || exit
 mkdir -p number
 cd number || exit
 touch assumptions.txt logical_connections.txt social_networks.txt maybe.txt
+echo "THIS PROGRAMM WAS MADE BY Tondo1313. https://github.com/Tondo1313/Number" > LICENCE.txt
 mkdir leads
 echo "your system was done for osint working. Look at the /Documents"
