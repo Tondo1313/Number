656b3421bbe3d7f9dfbaa5d6728e2ed7c946fcf5
Tondo <aliss.kuper2@gmail.com>
2026-04-07 17:02:56 +0300

    Update Number.sh

--- Number.sh
+++ Number.sh
@@ -5,4 +5,5 @@ cd number || exit
 touch предположения.txt логические_связи.txt социальные_сети.txt возможные.txt
 echo "THIS PROGRAMM WAS MADE BY Tondo1313. https://github.com/Tondo1313/Number" > LICENCE.txt
 mkdir зацепки
+mkdir отчёты
 echo "your system was done for osint working. Look at the /Documents"
