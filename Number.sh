74b180454e53c89718944de8f2fc0a1437fdf4c2
Tondo <aliss.kuper2@gmail.com>
2026-04-08 22:02:12 +0300

    Update Number.sh

--- Number.sh
+++ Number.sh
@@ -1,9 +1,15 @@
-echo "Добро пожаловать в Number. Сейчас создадутся все нужные папки и файлы..."
 cd ~/Documents || exit
-mkdir -p number
+mkdir number
 cd number || exit
 touch предположения.txt логические_связи.txt социальные_сети.txt возможные.txt
 echo "THIS PROGRAMM WAS MADE BY Tondo1313. https://github.com/Tondo1313/Number" > LICENCE.txt
 mkdir зацепки
 mkdir отчёты
-echo "your system was done for osint working. Look at the /Documents"
+cd отчёты || exit
+mkdir 1
+mkdir 2
+mkdir 3
+mkdir 4
+mkdir 5
+mkdir общий отчёт
+echo "Done."
